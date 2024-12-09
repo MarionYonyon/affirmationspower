73256bbef1715570dd85bec72aa02d08e0e47d4c
MarionYonyon <myrvana.m@gmail.com>
2024-12-09 12:22:56 +0000

    removing unecessary code in QuoteArea

--- src/components/QuoteArea.jsx
+++ src/components/QuoteArea.jsx
@@ -2,18 +2,10 @@ import "../styles/QuoteArea.css";
 import AffirmationGenerator from "./AffirmationGenerator";
 import crossIcon from "../images/icon-cross-quote.png";
 import React, { useState } from "react";
-import useGoalAndProgress from "../hooks/useGoalAndProgress";
 import { logClickAction } from "../utils/firebaseHelpers";
 
 const QuoteArea = () => {
-  const { incrementProgress } = useGoalAndProgress();
   const [fetchTrigger, setFetchTrigger] = useState(false);
-
-  const handleYesClick = () => {
-    incrementProgress(1); // Increment by 1 for "Yes"
-    logClickAction("YesClick");
-    setFetchTrigger(!fetchTrigger); // Toggle to refresh affirmations
-  };
 
   const handleCrossClick = () => {
     logClickAction("CrossClick");
